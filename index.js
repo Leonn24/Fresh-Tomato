2bcd312ccd24d4bbc0f6222ec6f0d251c46d2951
Arya Saini <arya30012@gmail.com>
2024-02-01 19:32:12 -0500

    finished fetch

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
-
-async function getMovie() {
-    const response = await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=8828c04b`)
-    const movie = await response.json();
-    console.log(movie.Title);
-    console.log(movie.Genre);
-    console.log(movie.Actors);
-    console.log(movie.Plot);
-    console.log(movie.Released);
-    console.log(movie.Poster);
-    console.log(movie.imdbRating);
-}
-(async () => {
-    await getMovie();
-});
-
+async function getMovieInfo(movieName) {
+    const apiKey = '8828c04b';
+    const url = `http://www.omdbapi.com/?t=${movieName}&apikey=${apiKey}`;
+  
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+  
+      if (response.ok) {
+        return {
+          title: data.Title,
+          genre: data.Genre,
+          actors: data.Actors,
+          plot: data.Plot,
+          released: data.Released,
+          poster: data.Poster,
+          imdbRating: data.imdbRating,
+          imdbID: data.imdbID
+        };
+      } else {
+        throw new Error(data.Error);
+      }
+    } catch (error) {
+      console.error('Error fetching movie information:', error.message);
+      return null;
+    }
+  }
+  
+  getMovieInfo('ted').then((movie) => {
+    if (movie) {
+      console.log(
+        `Title: ${movie.title}, Genre: ${movie.genre}, Actors: ${movie.actors}, Plot: ${movie.plot}, Released: ${movie.released}, Poster: ${movie.poster}, IMDB Rating: ${movie.imdbRating}, IMDBID: ${movie.imdbID}`
+      );
+    }
+  });
