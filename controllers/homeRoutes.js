adc1cb1e331cd91df54e994637290f2db72eb31b
Leonn24 <82012128+Leonn24@users.noreply.github.com>
2024-02-01 21:02:54 -0500

    removed get route example

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,7 +2,3 @@ const router = require('express').Router();
 const { Movie, Review, User } = require('../models');
 const withAuth = require('../utils/auth')
 
-app.get('/example', (req, res) => {
-    // Render the 'example' view
-    res.render('example', { data: 'Some data to pass to the view' });
-});
