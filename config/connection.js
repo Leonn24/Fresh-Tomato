fb46766aa7285b8a2eb5392d3e74a4a09cfd3bc7
Leonn24 <82012128+Leonn24@users.noreply.github.com>
2024-02-12 02:30:49 -0500

    added css to my favorites page and reviews page

--- config/connection.js
+++ config/connection.js
@@ -7,13 +7,13 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
     {
-      host: '127.0.0.1',
+      host: 'localhost',
       dialect: 'mysql',
-      port: 3306,
-      username: 'root',
-      database: 'freshtomatoe_db',
-      password: 'password',
+      port: 3306
     }
   );
 }
